48ada5064581298929fedc5e780a9c048706683a
hhr <2627367147@qq.com>
2025-01-20 20:30:14 +0800

    2.1

---
+++ cpp/oop2/chaper 2/2.1 concept/1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class CRectangle{//定义了矩形类
+    public:
+    double len,wid;//成员变量（属性）：长，宽
+    void init(double len_,double wid_){//成员函数1：输入长宽
+        len=len_,wid=wid_;
+    }
+    double CArea(){//成员函数2：算面积
+        return len * wid;
+    }
+    double CPerimeter(){//成员函数3：算周长
+        return 2*(len+wid);
+    }
+};
+void R(CRectangle &r){
+    r.init(5,6);
+     cout << r.CArea() << ' ' << r.CPerimeter() << '\n';
+}
+int main(){
+    int len,wid;
+    cin >> len >> wid;
+    CRectangle r1,r2;//定义了两个对象r1,r2
+    r1.init(len,wid);//注意这里输入的len和wid是全局变量，全局变量和成员变量允许重名，成员函数操作的是成员变量
+    cout << r1.CArea() << ' ' << r1.CPerimeter() << '\n';
+//方法1：对象名.成员名
+    r1.len=3;
+    r2.init(3,4);
+//方法2：指针->成员名
+    CRectangle*rr1,*rr2;
+    rr1=&r1;
+    rr2=&r2;
+    rr1->len=3;
+    rr2->init(3,4);
+//方法3：引用名.成员名
+    R(r1);
+    cout << r1.CArea() << ' ' << r1.CPerimeter() << '\n';
+
+}
