084d48621ef306621bc58bb425e1cea133825445
hhr <2627367147@qq.com>
2025-01-23 09:40:45 +0800

    constructors&destroctors

---
+++ cpp/oop2/chaper 3/constructors&destroctors/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class Person{
+    public:
+    Person(){
+        cout << "Person 构造函数" << '\n';
+    }
+    ~Person(){
+        cout << "Person 析构函数" << '\n';
+    }
+};
+int main(){
+    Person p;//创建对象，调用析构函数
+    system("pause");//程序在这里停止，此时还没有调用析构函数
+    return 0;//程序运行结束时对象销毁，调用析构函数
+}
