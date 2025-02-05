78a770eca634c6f034a1c3fed68e19636baf53ae
hhr <2627367147@qq.com>
2025-02-05 19:52:36 +0800

    内存四区

---
+++ cpp/oop2/MemoryModel/3/main.cpp
@@ -0,0 +1,12 @@
+//堆区：由程序员控制其分配与释放，如果没有释放，则在程序运行结束时由操作系统自动释放
+#include <iostream>
+using namespace std;
+int* func(){
+    int *p = new int(10);//使用new关键字在堆区申请了一块内存，将数据10存放进去，返回该内存的地址，由p接受
+    return p;
+}
+int main(){
+    int *p = func();
+    cout << *p << '\n';//由于是在堆区上面开辟的，所以函数结束后不会销毁
+    delete p;
+}
--- cpp/oop2/chaper 7/virtual/main.cpp
+++ cpp/oop2/chaper 7/virtual/main.cpp
@@ -28,3 +28,24 @@ int main(){
     a.Student::m_name = "lisi";
     a.func();
 }
+//Teacher和Student会分别维护一个vbptr（虚基类指针），这个指针指向的是vbtable（虚基类表）,表中会记录派生类对象到虚基类子对象的偏移量
+//这样即使有多个路径继承了同一个虚基类，最终也只会有一个虚基类的实例存在，该虚基类的派生类会通过虚基类表的偏移量访问同一个虚基类子对象
+//假设我创建一个Assistant对象a,a只会包含一个Person的子对象，而从Student和Teacher中继承过来的部分会分别维护一个虚基类指针，
+//Student的虚基类指针会指向一个虚基类表，其中记录了Student子对象与Person子对象之间的偏移量，Teacher子对象同理
+
+
+
+
+
+// Assistant 对象 a 的内存布局：
+// +---------------------+
+// | Teacher::vbptr      | -> 指向 Teacher 的 vbtable：Teacher子对象与Person子对象之间的偏移量
+// +---------------------+
+// | Teacher::m_id       |
+// +---------------------+
+// | Student::vbptr      | -> 指向 Student 的 vbtable：Student子对象与Person子对象之间的偏移量
+// +---------------------+
+// | Student::m_num      |
+// +---------------------+
+// | Person::m_name      |
+// +---------------------+
